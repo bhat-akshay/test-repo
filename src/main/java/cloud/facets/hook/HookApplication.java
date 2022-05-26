931294081a3097d164dd0136f270c0653c38334e
bhat-akshay <98866604+bhat-akshay@users.noreply.github.com>
2022-05-26 12:43:34 +0530

    Update HookApplication.java

--- src/main/java/cloud/facets/hook/HookApplication.java
+++ src/main/java/cloud/facets/hook/HookApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+// App Name
 @SpringBootApplication
 public class HookApplication {
 
